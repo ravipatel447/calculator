5bddd70f1694ddb8becf9d8ff9c8b2e8864c0247
ravipatelsimform <ravi.patel@simformsolutions.com>
2023-02-03 13:37:17 +0530

    reminin work- functionality

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ const equal = document.querySelector(".equal");
 const display = document.getElementById("display");
 const bottomPart = document.querySelector(".bottom-part");
 const clear = document.querySelector(".clear");
+const trignomatry = document.querySelector("#trignomatry");
+const functions = document.querySelector("#functions");
+const functionDropDown = document.querySelector(".fun");
+const trignomatryDropDown = document.querySelector(".trig");
+const fun = document.querySelector(".fun");
+const trig = document.querySelector(".trig");
+const secTrig = document.querySelector(".secondTrig");
+const ddt = document.querySelectorAll(".ddt");
+// const secTrig = document.getElementsByClassName("2ndTrig");
+// const secFun = document.querySelector(".2nd-fun");
 let string = "";
+
+fun.addEventListener("click", (e) => {
+  const target = e.target;
+  e.stopPropagation();
+});
+trig.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const target = e.target;
+});
 const updateDisplay = (str) => {
   let screen = str
     .toString()
@@ -11,10 +30,10 @@ const updateDisplay = (str) => {
     .replaceAll("/", "÷");
   display.textContent = str == "" ? "0" : screen;
 };
-const factorial = function(number){
-    if(number<=1) return 1;
-    return number*factorial(number-1);
-}
+const factorial = function (number) {
+  if (number <= 1) return 1;
+  return number * factorial(number - 1);
+};
 bottomPart.addEventListener("click", function (e) {
   if (e.target.classList.contains("evalAble")) {
     string += e.target.value;
@@ -58,19 +77,17 @@ bottomPart.addEventListener("click", function (e) {
   } else if (e.target.classList.contains("modeX")) {
     string !== "" ? (string = eval(`Math.abs(${string})`)) : "";
     updateDisplay(string);
-  }else if (e.target.classList.contains("EXP")) {
-    updateDisplay((Number(string)).toExponential());
-  }else if (e.target.classList.contains("fact")) {
+  } else if (e.target.classList.contains("EXP")) {
+    updateDisplay(Number(string).toExponential());
+  } else if (e.target.classList.contains("fact")) {
     string !== "" ? (string = factorial(eval(`Math.abs(${string})`))) : "";
     updateDisplay(string);
   }
 });
-
 clear.addEventListener("click", function () {
   string = "";
   updateDisplay(string);
 });
-
 equal.addEventListener("click", function () {
   try {
     string = eval(string);
@@ -83,3 +100,35 @@ equal.addEventListener("click", function () {
     console.log(err.message);
   }
 });
+trignomatry.addEventListener("click", (e) => {
+  if (
+    trignomatryDropDown.classList.contains("active") &&
+    e.target.closest("#trignomatry")
+  ) {
+    trignomatryDropDown.classList.remove("active");
+  } else {
+    trignomatryDropDown.classList.add("active");
+  }
+});
+functions.addEventListener("click", (e) => {
+  if (
+    functionDropDown.classList.contains("active") &&
+    e.target.closest("#functions")
+  ) {
+    functionDropDown.classList.remove("active");
+  } else {
+    functionDropDown.classList.add("active");
+  }
+});
+secTrig.addEventListener("click", () => {
+  if (secTrig.classList.contains("active-blue")) {
+    secTrig.classList.remove("active-blue");
+  } else {
+    secTrig.classList.add("active-blue");
+    ddt.forEach((e) => {
+      e.innerHTML = e.dataset["2nd"];
+    });
+  }
+});
+
+// console.log(secTrig, "sdkfsdkfwisefisdfjn");
